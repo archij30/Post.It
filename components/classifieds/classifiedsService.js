8f5cf0640bb5859ffe724440dd67600bc37befbf
Archi Jain <archij30@gmail.com>
2020-09-29 02:59:16 +0530

    Adding firebase

--- components/classifieds/classifiedsService.js
+++ components/classifieds/classifiedsService.js
@@ -1,12 +1,18 @@
 (function() {
 
-    var ClassifiedsService = function($http) {
+    var ClassifiedsService = function($http, $firebaseArray) {
 
         this.getClassifieds = function() {
             return $http.get("data/classifieds.json");
         }
+        var ref = firebase.database().ref();
+        this.getFirebaseRef = function() {
+            return {
+                ref: $firebaseArray(ref)
+            }
+        }
 
     }
-    ClassifiedsService.$inject = ["$http"];
+    ClassifiedsService.$inject = ["$http", "$firebaseArray"];
     angular.module("ngClassifieds").service("classifiedsService", ClassifiedsService);
 }());
--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 (function() {
-    let ngClassifiedsApp = angular.module("ngClassifieds", ["ngMaterial", "ngAnimate", "ui.router"]);
+    let ngClassifiedsApp = angular.module("ngClassifieds", ["ngMaterial", "ngAnimate", "ui.router", "firebase"]);
     ngClassifiedsApp.config(function($mdThemingProvider, $stateProvider) {
         //changing default theme to blue
         $mdThemingProvider.theme('default')
@@ -23,5 +23,20 @@
                     classified: null
                 }
             });
+
+        var firebaseConfig = {
+            apiKey: "AIzaSyA2hlxG8JRVZg_Kk5ChVZAWN7eE6v8oxLw",
+            authDomain: "ngclassifieds-7d6fd.firebaseapp.com",
+            databaseURL: "https://ngclassifieds-7d6fd.firebaseio.com",
+            projectId: "ngclassifieds-7d6fd",
+            storageBucket: "ngclassifieds-7d6fd.appspot.com",
+            messagingSenderId: "419063773596",
+            appId: "1:419063773596:web:a8a74fac2fa0c24bc2b059",
+            measurementId: "G-EQ6W5B431D"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+
+
     });
 }());
